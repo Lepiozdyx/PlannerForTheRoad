2976e757d8f565507d001d36f6fb45ccc4f0fcfd
Developer <Lepiozdyx@gmail.com>
2026-02-26 15:44:08 -0300

    Initial Commit

---
+++ PlannerForThe Road/PlannerForThe_RoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerForThe_RoadApp.swift
+//  PlannerForThe Road
+//
+//  Created by Alex on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlannerForThe_RoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
